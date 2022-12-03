77e7c676a0ff7a798614a9a4a2d1962014966bd6
biancapower <bianca@biancapower.com>
2022-12-03 19:55:15 +1100

    Solve day-03-01

---
+++ 2022/day-03-1.rb
@@ -0,0 +1,26 @@
+require 'pry'
+
+file = File.open("day-03-input.txt")
+file_data = file.readlines.map{|l| l.strip.split('')}
+
+items = []
+
+file_data.each do |x|
+  # find intersection of first half and second half
+  items << x[0...x.length/2].intersection(x[x.length/2..-1])
+end
+
+=begin
+"A" ->  "Z"
+65  ->  90
+
+"a" ->  "z"
+97  ->  122
+=end
+
+items.map! do |i|
+  s = i[0].sum
+  s > 96 ? s - 96 : s - 38
+end
+
+p items.sum
